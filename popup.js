f9f18810a98dda61d31a68848cdfcfedbfd1dae9
0xCeefu <0xCeefu@gmail.com>
2025-08-03 22:06:06 +0100

    add break, duration input, fix update UI bug

--- popup.js
+++ popup.js
@@ -1,22 +1,42 @@
 const startButton = document.getElementById('start');
 const stopButton = document.getElementById('stop');
 const resetButton = document.getElementById('reset');
+const focusDurationInput = document.getElementById('focus-duration');
+const breakDurationInput = document.getElementById('break-duration');
 
-let timeLeft = 0.1 * 60;
+let isFocus = true;
+let focusDuration = focusDurationInput.value;
+let breakDuration = breakDurationInput.value;
+let timeLeft = 0;
+let pomodoroActive = false;
+
 let timerInterval = null;
 
 function updateTimerDisplay() {
-    const minutes = Math.floor(timeLeft / 60);
-    const seconds = timeLeft % 60;
-    document.getElementById('timer').textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+    if (pomodoroActive) {
+        const minutes = Math.floor(timeLeft / 60);
+        const seconds = timeLeft % 60;
+        document.getElementById('timer').textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+    } else {
+        const minutes = Math.floor((focusDuration * 60) / 60);
+        const seconds = (focusDuration * 60) % 60;
+        document.getElementById('timer').textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+    }
+
 }
 
 function startTimer() {
     if (timerInterval) return;
-    if (timeLeft <= 0) {
-        showNotification("Pomodoro session complete! Reset the timer to start a new session.");
-        return;
+
+    pomodoroActive = true
+    if (isFocus && timeLeft <= 0) {
+        timeLeft = focusDuration * 60;
+        console.log("Focus");
+    } else if (!isFocus && timeLeft <= 0) {
+        timeLeft = breakDuration * 60;
+        console.log("Break");
     }
+
     timerInterval = setInterval(() => {
         timeLeft--;
         updateTimerDisplay();
@@ -25,6 +45,8 @@ function startTimer() {
             clearInterval(timerInterval);
             timerInterval = null;
             showNotification("Pomodoro session complete!");
+            isFocus = !isFocus;
+            startTimer();
         }
     }, 1000);
 }
@@ -45,7 +67,9 @@ function showNotification(message) {
 
 function resetTimer() {
     pauseTimer();
-    timeLeft = 0.1 * 60;
+    timeLeft = 0;
+    isFocus = true;
+    pomodoroActive = false
     updateTimerDisplay();
 }
 
@@ -59,6 +83,16 @@ stopButton.addEventListener('click', () => {
 
 resetButton.addEventListener('click', () => {
     resetTimer();
+});
+
+focusDurationInput.addEventListener('change', () => {
+    focusDuration = focusDurationInput.value;
+    updateTimerDisplay();
+});
+
+breakDurationInput.addEventListener('change', () => {
+    breakDuration = breakDurationInput.value;
+    updateTimerDisplay();
 });
 
 updateTimerDisplay()
